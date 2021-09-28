212faf0d1b47e11d1218de9f7dc4bd7ebb674791
josecarlosgp <jotace300@gmail.com>
2021-09-28 18:26:11 +0200

    Agregar archivos de proyecto.

---
+++ FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextoUsuarioTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextoUsuarioTextBox.Text.Length > 46)
+            {
+                ResultadoTextBlock.Text = TextoUsuarioTextBox.Text.Substring(0,46) + "...";
+            }
+            else
+            {
+                ResultadoTextBlock.Text = TextoUsuarioTextBox.Text;
+            }
+        }
+
+        private void AzulRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.Foreground = Brushes.Blue;
+        }
+
+        private void RojoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.Foreground = Brushes.Red;
+        }
+
+        private void VerdeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+            ResultadoTextBlock.Foreground = Brushes.Green;
+        }
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+        private void CursivaTextBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+        private void CursivaTextBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ResultadoTextBlock.FontStyle = FontStyles.Normal;
+        }
+    }
+}
